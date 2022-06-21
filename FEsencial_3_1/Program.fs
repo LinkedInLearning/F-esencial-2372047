195639e90981eb862cc7c742f7d0b6ea393557de
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:27:36 -0500

    Ejercicios 04_01

---
+++ FEsencial_3_1/Program.fs
@@ -0,0 +1,13 @@
+﻿(*
+match expresion with
+| patron [ when condicion] -> expresion-resultado
+| patron2 [ when condicion2] -> expresion-resultado
+*)
+open System
+printfn "Elige una opc 1-saludo, 2-despedida"
+let menu = Console.ReadLine()
+let accion =
+ match menu with
+  | "1" -> printfn "Hola"
+  | "2" -> printfn "Adios"
+  | _ -> printfn "Opción inválida"
