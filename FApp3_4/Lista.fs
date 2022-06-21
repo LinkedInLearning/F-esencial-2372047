92dce4cd8b1d6639f3387a306b6b6b26de3e3d4d
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:23:58 -0500

    Ejercicios 03_04

---
+++ FApp3_4/Lista.fs
@@ -0,0 +1,6 @@
+﻿module Lista
+
+let ciudades = [ "Barcelona"; "Madrid"; "Valencia"; "Toledo" ]
+
+let items = [ -55; 22; 48; -99; 85; 13; -2 ]
+
---
+++ FApp3_4/Program.fs
@@ -0,0 +1,22 @@
+﻿open Lista
+open System
+
+let cantidadCds = ciudades.Length
+
+let esVaciaCds = ciudades.IsEmpty
+
+printfn "Ingresa una ciudad"
+
+let cd = Console.ReadLine()
+
+let existeCd cd cds = List.exists (fun el -> el = cd) cds
+let existe = existeCd cd ciudades
+printfn $"{existe}"
+
+
+let cdsOrd = List.sort(ciudades)
+printfn "%A" ciudades
+printfn "%A" cdsOrd
+
+let itemsOrd = List.sortBy(fun el -> abs el) items
+printfn "%A" itemsOrd
