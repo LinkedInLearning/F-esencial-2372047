a75f3b538aae14e9d321a9e2e868b1852e3aa64a
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:14:43 -0500

    Ejercicios 02_04

--- Program.fs
+++ Program.fs
@@ -10,22 +10,15 @@ let restar x =
     let total =  x - 10
     total
 
-//otra forma de definir funciones
-let s x y = x + y + 10
-
-//anotaciones de tipo
-let sum (x: uint32) (y: uint32) = x + y + 10u
-
-//anotaciones de tipo para valor de retorno
-let suma x y : uint32 = x + y + 100u
+let suma x y = x + y + 10
     
 [<EntryPoint>]
 let main argv=
     let saludo = "Hola mundo F#!"
     imprimeDespedida
-    let num = 5
-    //anotacion de tipo
-    //let num : float = 5.0
-    let numero = 5 + 5
-    printfn "%s" saludo
+//    let miTotal = suma 10 20 //no mutable
+    let mutable miTotal = suma 10 20
+    printfn "%i" miTotal
+    miTotal = 50
+    printfn "%i" miTotal
     0
