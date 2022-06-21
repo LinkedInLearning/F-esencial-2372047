b65095fd784743fe7fdda7cb1673dc60adbf4a5d
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:31:37 -0500

    Ejercicios 04_05

---
+++ FEsencial_3_5/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.Text.RegularExpressions
+
+//failwith
+//invalidArg
+//nullArg
+//invalidOp
+
+//patron de variable
+let verificarCorreo (correo:string) =
+ if correo.Length > 5 then
+  match correo with
+   | c when Regex.Match(c,@".+@example.+").Success ->
+    printfn "Correo es de example"
+   | c when Regex.Match(c,@".+@microsoft.+").Success ->
+    printfn "Correo es de microsoft"
+   | c when Regex.Match(c,@".+@.+").Success ->
+    printfn "Correo de proveedor no identificado"
+   | _ ->
+    printfn "Formato de correo incorrecto"
+ //else invalidArg $"correo invalido" $"El argumento correo es invalido {correo}"
+ else
+  raise (new InvalidOperationException($"El argumento correo es invalido {correo}"))
+
+verificarCorreo "noemi"
