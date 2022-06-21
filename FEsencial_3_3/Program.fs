3377dfef76a2e46e2826cc4f699e1f63919abcf3
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:28:52 -0500

    Ejercicios 04_03

---
+++ FEsencial_3_3/Program.fs
@@ -0,0 +1,20 @@
+﻿(*
+let rec funcion param1 param2 =
+ cuerpo
+ *)
+
+
+open System 
+let rec fact n =
+ if n < 1 then 1
+ else n * fact (n-1)
+
+let usuarioDato = Console.ReadLine()
+let num = usuarioDato |> int
+printfn $"El factorial de {num} = {fact num}"
+
+let rec factB = function
+ | n when n < 1 -> 1
+ | n -> n * fact (n-1)
+
+printfn $"El factorial de {num} = {factB num}"
