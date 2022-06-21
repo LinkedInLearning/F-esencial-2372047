6a5e0131b6cb924a3939f149e21aa7d37be291ca
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:25:04 -0500

    Ejercicios 03_05

--- FApp3_4/Lista.fs
+++
@@ -1,6 +0,0 @@
-﻿module Lista
-
-let ciudades = [ "Barcelona"; "Madrid"; "Valencia"; "Toledo" ]
-
-let items = [ -55; 22; 48; -99; 85; 13; -2 ]
-
---
+++ Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+module Tupla =
+    //tipos mixtos
+    let ejmTupla = ("elemento 1", "el 2", 2, 5.0 )
+
+    //tupla de expresiones
+    let tuplaExp = (5+5, "hola "+ "mundo", 8*4)
+
+    //titulo, duracion, instructor, vistas
+    let curso = ("F#", 3.2, "Noemi Leon", 5000)
+    let titulo, duracion, instructor, vistas = curso
+    printfn "El titulo del curso es %s" titulo
+    //ignorar elementos con _
+    let _, _, inst, vist = curso
+    printfn "Instructor %s, vistas %i" inst vist
+
+    //funcion que retorna una tupla
+    let divVals x y = 
+        let resultado = x/y
+        let residuo = x%y
+        (resultado, residuo)
+
+    let divResultado, divResiduo = divVals 5 2
+    printfn "Resultado = %i, Residuo = %i" divResultado divResiduo
