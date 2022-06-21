00114bad89d40dcfef6a2f2b59d7712a79ca8f0f
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:21:05 -0500

    Ejercicios 03_01

---
+++ FEsencial3_1/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+(* if condicion then
+	//bloque 
+//else
+    //bloque *)
+
+Console.WriteLine("Ingresa tu nombre")
+let nombre = System.Console.ReadLine()
+let caract = nombre.Length
+
+if caract < 5 then
+    printfn "Nombre corto"
+else
+    printfn "Nombre largo"
+
+let tipoNombre c =
+    if c < 5 then "Nombre corto"
+    elif c = 5 then "Nombre mediano"
+    else "Nombre largo"
+
+printfn $"{nombre} es {tipoNombre caract}"
