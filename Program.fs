2656dceabae8a3df5101839cfc83857219fb6014
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:11:56 -0500

    Ejercicios 02_03

---
+++ Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let imprimeDespedida() =
+    let num = 1
+    let msj = "Vuelve pronto"
+    printfn "%s" msj
+
+let restar x =
+    let total =  x - 10
+    total
+
+//otra forma de definir funciones
+let s x y = x + y + 10
+
+//anotaciones de tipo
+let sum (x: uint32) (y: uint32) = x + y + 10u
+
+//anotaciones de tipo para valor de retorno
+let suma x y : uint32 = x + y + 100u
+    
+[<EntryPoint>]
+let main argv=
+    let saludo = "Hola mundo F#!"
+    imprimeDespedida
+    let num = 5
+    //anotacion de tipo
+    //let num : float = 5.0
+    let numero = 5 + 5
+    printfn "%s" saludo
+    0
