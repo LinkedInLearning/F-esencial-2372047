fe2458b8e7564bf43230cbac08ee07b413e60094
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:33:37 -0500

    Ejercicios 05_01

---
+++ FEsencial_5_1/Program.fs
@@ -0,0 +1,31 @@
+﻿
+type Curso(titulo: string, duracion: int, repo: string) =
+ 
+ let mutable duracionT = duracion
+ let mutable repositorio = ""
+ let mutable vistas = 0
+
+ //solo lectura
+ member this.Vistas = vistas
+
+ //solo escritura
+ member this.Repositorio with set (repo) = repositorio <- repo
+
+ //lectura y escritura
+ member val Titulo = titulo
+ member this.Duracion
+  with get() = duracionT
+  and set (durac) = duracionT <- durac
+
+
+let titulo = "F#"
+let duracion = 3
+let rutaRepo = "https://github.com/noeleo25"
+let miCurso = Curso(titulo, duracion, rutaRepo)
+
+//acceso a propiedades
+printfn $"{miCurso.Vistas}"
+printfn $"{miCurso.Duracion}"
+
+miCurso.Duracion <- 5
+printfn $"{miCurso.Duracion}"
