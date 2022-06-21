858f12c396b6251b4b267c087eb3254b8e6a98e0
Noemi Leon <noemi.telematik213@gmail.com>
2022-06-20 20:22:57 -0500

    Ejercicios 03_03

--- FAppLoops/Arreglo.fs
+++ FAppLoops/Arreglo.fs
@@ -1,4 +1,6 @@
-﻿let coordA = [| -0.5; 20.5; -40.0 |]
+﻿module Arreglo
+
+let coordA = [| -0.5; 20.5; -40.0 |]
 
 let coordB = 
     [|
@@ -11,7 +13,7 @@ let productos = [| "a"; "b"; "c" |]
 let el = productos[0]
 
 let pares = Array.init 10 (fun i -> i * 2)
-printfn "%A" pares
+//printfn "%A" pares
 
 let vacioArr = Array.empty
 
@@ -19,11 +21,10 @@ let vacioArr = Array.empty
 let coordAB = Array.concat [ coordA; coordB ]
 
 let cuadrados = [| for i in 1 .. 5 -> i*i|]
-printfn "%A" cuadrados
+//printfn "%A" cuadrados
 
-printfn "%A" (Array.filter(fun el -> el % 2 = 0) cuadrados)
+//printfn "%A" (Array.filter(fun el -> el % 2 = 0) cuadrados)
 
 let rango = productos[0..2]
 let reversaProd = Array.rev productos
 let prodB = Array.append productos [| "d"; "e" |]
-
